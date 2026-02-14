9dcd65e6be83c934f682241d58aef10b9a9d4a50
Lauta <154246130+lautaguardati@users.noreply.github.com>
2026-02-13 23:23:40 -0300

    Revisa la base de datos
    revisa que esté creada la base de datos. Sino lo está la crea.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
-#include "producto.h"
 #include "empresa.h"
+#include "producto.h"
+#include <fstream>
 using namespace std;
+
+bool revisarExistenciaArchivo (string direc) {
+	ifstream archivo (direc, ios::binary);
+	return archivo.good();
+}
+
+
+
+
 
 int main() {
 	
+	if (revisarExistenciaArchivo("lista_prov.dat")) {
+		ofstream cargaInicial("lista_prov.dat", ios::binary);
+	} else {
+		ofstream crear("lista_prov.dat", ios::binary);
+	}
 	
+	ifstream cargaInicial("lista_prov.dat", ios::binary | ios::in);
 	
-	
-	
-	
-	
-	
-	
-	
-	
+	if (!cargaInicial.is_open()) {
+		throw runtime_error("No se pudo abrir el archivo");
+	}
+
+
 	
 	
 	
